0585509929a9d2f4c86df06cfaafeef2d1c133b7
Lucas Amaral Velasque <81877529+lucasamaral296@users.noreply.github.com>
2023-07-24 20:57:03 -0300

    adicionando passo a passo

---
+++ main.py
@@ -0,0 +1,22 @@
+# Criar o projeto no Firebase
+
+# Criar o Database (atenção as regras de segurança)
+
+# Estrutura da árvore
+
+# Interação com o Database (REST API)
+
+# Criar uma venda (POST)
+
+# Criar um novo produto (POST)
+
+# Edtar a venda (PATCH)
+
+# Pegar uma venda especifica ou todas as vendas (GET)
+
+# Deletar uma venda (DELETE)
+
+# O que seria de legal após isso?
+
+# Autenticação
+
