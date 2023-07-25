f10f2e1e0ebec4cf9ae626a32634c1e88dbb1786
Lucas Amaral Velasque <81877529+lucasamaral296@users.noreply.github.com>
2023-07-24 21:48:09 -0300

    criando no banco de dados (POST)

--- main.py
+++ main.py
@@ -6,9 +6,26 @@
 
 # Interação com o Database (REST API)
 
+import requests
+import json 
+
+# criando um arquivo para armazenar o link do database
+from dbfirebase import linkdb
+
+
 # Criar uma venda (POST)
 
+dados = {'Cliente': 'Estefany', 'Preco': 150, 'Produto': 'Teclado'}
+requisicao = requests.post(f'{linkdb}/Vendas/.json', data=json.dumps(dados))
+print(requisicao)
+print(requisicao.text)
+
 # Criar um novo produto (POST)
+
+dados = {'Nome': 'Teclado', 'Preco': 150, 'Quantidade': '10'}
+requisicao = requests.post(f'{linkdb}/Produtos/.json', data=json.dumps(dados))
+print(requisicao)
+print(requisicao.text)
 
 # Edtar a venda (PATCH)
 
