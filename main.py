fa9687020317ae6bb3387631b9bfda966d5dc71b
Lucas Amaral Velasque <81877529+lucasamaral296@users.noreply.github.com>
2023-07-25 08:15:41 -0300

    Fazendo GET no database

--- main.py
+++ main.py
@@ -13,14 +13,14 @@ import json
 from dbfirebase import linkdb
 
 
-# # Criar uma venda (POST)
+# Criar uma venda (POST)
 
 # dados = {'Cliente': 'Estefany', 'Preco': 150, 'Produto': 'Teclado'}
 # requisicao = requests.post(f'{linkdb}/Vendas/.json', data=json.dumps(dados))
 # print(requisicao)
 # print(requisicao.text)
 
-# # Criar um novo produto (POST)
+# Criar um novo produto (POST)
 
 # dados = {'Nome': 'Teclado', 'Preco': 150, 'Quantidade': '10'}
 # requisicao = requests.post(f'{linkdb}/Produtos/.json', data=json.dumps(dados))
@@ -29,13 +29,23 @@ from dbfirebase import linkdb
 
 # Edtar a venda (PATCH)
 
-dados = {'Cliente': 'Jorge'}
-requisicao = requests.patch(f'{linkdb}/Vendas/-Na9UnucdRt9ntN3Wtfq/.json', data=json.dumps(dados))
-print(requisicao)
-print(requisicao.text)
+# dados = {'Cliente': 'Jorge'}
+# requisicao = requests.patch(f'{linkdb}/Vendas/-Na9UnucdRt9ntN3Wtfq/.json', data=json.dumps(dados))
+# print(requisicao)
+# print(requisicao.text)
 
 # Pegar uma venda especifica ou todas as vendas (GET)
 
+requisicao = requests.get(f"{linkdb}/Vendas/.json")
+print(requisicao)
+dic_requisicao = requisicao.json()
+id_estefany = None
+for id_venda in dic_requisicao:
+    cliente = dic_requisicao[id_venda]['Cliente']
+    if cliente == "Estefany":
+        id_estefany = id_venda
+        print(id_venda)
+        
 # Deletar uma venda (DELETE)
 
 # O que seria de legal após isso?
