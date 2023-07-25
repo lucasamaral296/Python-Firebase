671950458e8e8ab50371e534d4b1079ca14e0cd7
Lucas Amaral Velasque <81877529+lucasamaral296@users.noreply.github.com>
2023-07-25 07:55:52 -0300

    Editando (Patch)

--- main.py
+++ main.py
@@ -13,21 +13,26 @@ import json
 from dbfirebase import linkdb
 
 
-# Criar uma venda (POST)
+# # Criar uma venda (POST)
 
-dados = {'Cliente': 'Estefany', 'Preco': 150, 'Produto': 'Teclado'}
-requisicao = requests.post(f'{linkdb}/Vendas/.json', data=json.dumps(dados))
-print(requisicao)
-print(requisicao.text)
+# dados = {'Cliente': 'Estefany', 'Preco': 150, 'Produto': 'Teclado'}
+# requisicao = requests.post(f'{linkdb}/Vendas/.json', data=json.dumps(dados))
+# print(requisicao)
+# print(requisicao.text)
 
-# Criar um novo produto (POST)
+# # Criar um novo produto (POST)
 
-dados = {'Nome': 'Teclado', 'Preco': 150, 'Quantidade': '10'}
-requisicao = requests.post(f'{linkdb}/Produtos/.json', data=json.dumps(dados))
-print(requisicao)
-print(requisicao.text)
+# dados = {'Nome': 'Teclado', 'Preco': 150, 'Quantidade': '10'}
+# requisicao = requests.post(f'{linkdb}/Produtos/.json', data=json.dumps(dados))
+# print(requisicao)
+# print(requisicao.text)
 
 # Edtar a venda (PATCH)
+
+dados = {'Cliente': 'Jorge'}
+requisicao = requests.patch(f'{linkdb}/Vendas/-Na9UnucdRt9ntN3Wtfq/.json', data=json.dumps(dados))
+print(requisicao)
+print(requisicao.text)
 
 # Pegar uma venda especifica ou todas as vendas (GET)
 
